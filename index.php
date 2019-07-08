df5f6cd3930d2b2c4d7feeb9396ef668f61a071f
kk300 <kk300.427@gmail.com>
2019-07-08 18:10:44 +0900

    Add source code

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>スポーツボウリング応援サイトSplash!</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="format-detection" content="telephone=no, email=no, address=no">
+        <meta name="description" content="プロボウラーのスケジュールをブログなどから集めてまとめています！">
+        <meta name="keywords" content="ボウリング, プロボウラー, チャレンジ, スケジュール">
+        <meta itemprop="name" content="スポーツボウリング応援サイトSplash!">
+        <meta itemprop="description" content="プロボウラーのスケジュールをブログなどから集めてまとめています！">
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:title" content="スポーツボウリング応援サイトSplash!">
+        <meta name="twitter:url" content="http://splash.mybluemix.net/index.php">
+        <meta name="twitter:description" content="プロボウラーのスケジュールをブログなどから集めてまとめています！">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="スポーツボウリング応援サイトSplash!">
+        <meta property="og:url" content="http://splash.mybluemix.net/index.php">
+        <meta property="og:site_name" content="スポーツボウリング応援サイトSplash!">
+        <meta property="og:description" content="プロボウラーのスケジュールをブログなどから集めてまとめています！">
+        
+        <!-- jQuery-->
+        <!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script> -->
+        <script type="text/javascript" src="./js/jquery-3.1.1.min.js"></script>
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="./bootstrap/bootstrap.min.css">
+        
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="./bootstrap/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="./bootcards/dist/css/bootcards-desktop.min.css">
+        <link rel="stylesheet" href="./bootcards/dist/css/bootcards-desktop.css.map">
+        
+        <style type="text/css">
+            h1 {
+                position: relative;
+                padding-top: .10em;
+                padding-bottom: .5em;
+                color: #34db58;
+                // background-color: #333333;
+                border-bottom: 10px solid #ccc;
+                
+            }
+            h1::after {
+                position: absolute;
+                bottom: -10px;
+                left: 0;
+                z-index: 2;
+                content: '';
+                width: 20%;
+                height: 10px;
+                background-color: #34db58;
+            }
+        </style>
+        
+        <!-- Latest compiled and minified JavaScript -->
+        <script type="text/javascript" src="./bootstrap/bootstrap.min.js"></script>
+        <script type="text/javascript" src="./bootcards/dist/js/bootcards.min.js"></script>
+        <script type="text/javascript" src="./js/Schedule2.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="header">
+                <h1><b>Splash!</b></h1>
+                <p>プロボウラーチャレンジスケジュールサイト</p>
+                <nav class="navbar navbar-default" role="navigation" style="background-color: 0x000000;">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">MENU</a>
+                    </div> <!-- navbar-header -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="./index.html"><b>ホーム</b></a></li>
+                            <li><a href="./index.html"><b>スケジュール</b></a></li>
+                            <li><a href="./index.html"><b>リンク</b></a></li>
+                        </ul>
+                    </div> <!-- collapse -->
+                </nav> <!-- navbar -->
+            </div> <!-- header -->
+        </div> <!-- container -->
+        
+        <div class="container">
+
+            <div class="contents">
+
+                <div class="row">
+
+                    <div class="hidden-xs col-sm-3 col-md-3">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr><th align="left">選手のブログへ</th></tr>
+                            </thead>
+                            <tbody>
+                                <tr><td align="left">　<a href="http://ameblo.jp/a2aoki/" target="_blank"><font size="2">青木彰彦プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/kaechan-1019/" target="_blank"><font size="2">秋光楓プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://www.starlanes.co.jp/asada/" target="_blank"><font size="2">浅田梨奈プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-hitomiandoh" target="_blank"><font size="2">安藤瞳プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/iguchi-pro/" target="_blank"><font size="2">井口直之プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blogs.yahoo.co.jp/yuki324jpba" target="_blank"><font size="2">市原由紀プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/eueueueueueueueueu/" target="_blank"><font size="2">岩見彩乃プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/chitose-u/" target="_blank"><font size="2">生方千登勢プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-minaendoh" target="_blank"><font size="2">遠藤未菜プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/yfbnao/" target="_blank"><font size="2">大石奈緒プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/otaako/" target="_blank"><font size="2">太田朱虹プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/oneya-ai/" target="_blank"><font size="2">大根谷愛プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/to-pro-0413-0486/" target="_blank"><font size="2">大谷内高志プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blogs.yahoo.co.jp/okadaikuko307" target="_blank"><font size="2">岡田郁子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://kamiyabowling.dosugoi.net/" target="_blank"><font size="2">神谷昇司プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/tomomi422/" target="_blank"><font size="2">亀井智美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://profile.ameba.jp/viviycha/" target="_blank"><font size="2">川崎由意プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/kawafunemanami/" target="_blank"><font size="2">川舩愛美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/rtailkiss/" target="_blank"><font size="2">菊地葵プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-yukakishida" target="_blank"><font size="2">岸田有加プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/kubotaayaka/" target="_blank"><font size="2">久保田彩花プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/yas792/" target="_blank"><font size="2">黒川靖プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/kuwatomiki/" target="_blank"><font size="2">桑藤美樹プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/koizuminatsumi/" target="_blank"><font size="2">小泉奈津美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://s.maho.jp/blog/df3c63g6956a96d3/" target="_blank"><font size="2">小金正治プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/kobayashiayumi/" target="_blank"><font size="2">小林あゆみプロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/tetsuyakobayashi1198/" target="_blank"><font size="2">小林哲也プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/kobayashiyoshimi/" target="_blank"><font size="2">小林よしみプロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/grandbowl-saitoh/" target="_blank"><font size="2">斉藤茂雄プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/saitoseiya/" target="_blank"><font size="2">齋藤征哉プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-mikasakai" target="_blank"><font size="2">酒井美佳プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/5962314/" target="_blank"><font size="2">坂本詩緒里プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-asamisakurai" target="_blank"><font size="2">櫻井麻美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-marikosakurai" target="_blank"><font size="2">櫻井眞利子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/jpba1091/" target="_blank"><font size="2">佐取賢プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blogs.yahoo.co.jp/junko_467" target="_blank"><font size="2">白石順子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/grandbowl-suzuki/" target="_blank"><font size="2">鈴木亜季プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/don-chuck-pro/" target="_blank"><font size="2">鈴木博喜プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-risasuzuki" target="_blank"><font size="2">鈴木理沙プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/susumu-hiromi/" target="_blank"><font size="2">進博美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/stephanypyrdol/" target="_blank"><font size="2">ステファニー・ピヤドールプロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/takeharamiki/" target="_blank"><font size="2">竹原三貴プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-amitanaka" target="_blank"><font size="2">田中亜実プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/grandbowl-tanigawa/" target="_blank"><font size="2">谷川章子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/tsurui-anami/" target="_blank"><font size="2">鶴井亜南プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://bowling.sugai-dinos.jp/pc/blog/" target="_blank"><font size="2">寺下智香プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/masami-abe369/" target="_blank"><font size="2">堂元美佐プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://profile.ameba.jp/nilawatit/" target="_blank"><font size="2">殿井ニラワティプロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/yuko-pro/" target="_blank"><font size="2">中谷優子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-marikonakano" target="_blank"><font size="2">中野麻理子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.livedoor.jp/jpba_nakamura/" target="_blank"><font size="2">中村智プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/jpba-1247/" target="_blank"><font size="2">中村太亮プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://p-mitsukinakamura.exblog.jp/" target="_blank"><font size="2">中村美月プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/aki-nawa/" target="_blank"><font size="2">名和秋プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/jpba-431/" target="_blank"><font size="2">野瀬千春プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-masamihasegawa" target="_blank"><font size="2">長谷川真実プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://kenji1254.exblog.jp/" target="_blank"><font size="2">馬場健司プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://www.futababowl.jp/himeji/index.html" target="_blank"><font size="2">姫路麗プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/p-chan-300/" target="_blank"><font size="2">福原明美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blogs.yahoo.co.jp/fujitama_330" target="_blank"><font size="2">藤田麻衣プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-maifunamoto" target="_blank"><font size="2">舟本舞プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://shokobowling.dosugoi.net/" target="_blank"><font size="2">古田翔子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/jpba947/" target="_blank"><font size="2">保倉映義プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://zassoudamashi.blog65.fc2.com/" target="_blank"><font size="2">堀内綾プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/horiesan/" target="_blank"><font size="2">堀江真一プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/narumi19900203/" target="_blank"><font size="2">本間成美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-rumimaeya" target="_blank"><font size="2">前屋瑠美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/ryoko492/" target="_blank"><font size="2">町井綾子プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://kmcitybowl.p-kit.com/" target="_blank"><font size="2">松永裕美プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/murayamaeri/" target="_blank"><font size="2">村山恵梨プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/mochizukirie/" target="_blank"><font size="2">望月理江プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.excite.co.jp/p-sanaemori" target="_blank"><font size="2">森彩奈江プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://bigjun.blog.so-net.ne.jp/" target="_blank"><font size="2">矢島純一プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/jpba447-miho/" target="_blank"><font size="2">柳美穂プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/yamadayuki/" target="_blank"><font size="2">山田幸プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://ameblo.jp/ty1060/" target="_blank"><font size="2">吉川朋絵プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://jpba848.jugem.jp/" target="_blank"><font size="2">吉田文啓プロ</font></a></td></tr>
+                                <tr><td align="left">　<a href="http://blog.rankseeker.net/watanabekeaki/" target="_blank"><font size="2">渡辺けあきプロ</font></a></td></tr>
+                            </tbody>
+                        </table>
+                    </div> <!-- col -->
+
+                    <div class="col-xs-12 col-sm-9 col-md-6 ">
+                        <table id="schedule" class="table class">
+                            <tbody>
+                                <tr><th>日</th><th>スケジュール</th></tr>
+                                <tr><th>1日</th><th></th></tr>
+                            </tbody>
+                        </table>
+                    </div> <!-- col -->
+                    
+                    <div class="hidden-xs hidden-sm col-md-3 ">
+                        <div>広告<br />
+                            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                            <!-- test_unit01 -->
+                            <ins class="adsbygoogle"
+                                 style="display:inline-block;width:300px;height:250px"
+                                 data-ad-client="ca-pub-1068622611384585"
+                                 data-ad-slot="6545396557"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div><!-- 広告 -->
+                    </div> <!-- col -->
+                </div> <!-- row -->
+            </div> <!-- contents -->
+        </div> <!-- container -->
+        
+        <div class="container">
+            <div class="footer">
+                <hr>
+                <center>2013-2017 MKProject</center>
+                <hr>
+            </div> <!-- footer -->
+        </div> <!-- container -->
+    </body>
+</html>
